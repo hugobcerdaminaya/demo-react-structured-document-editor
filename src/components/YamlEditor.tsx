a7b1eee7480adc98f3aef1efdd413eed8a989a46
Hugo B. Cerda Minaya <hcerda@strappcorp.com>
2022-10-18 03:21:25 -0500

    implements action to update document

--- src/components/YamlEditor.tsx
+++ src/components/YamlEditor.tsx
@@ -1,19 +1,53 @@
-import * as React from "react";
 import { db } from "../models/db";
 import { useState } from 'react';
-import { useLiveQuery } from 'dexie-react-hooks';
+import Ajv, {JSONSchemaType} from "ajv";
+import { YamlList } from '../components/YamlList';
+import { Error } from '../components/Error';
 
 export function YamlEditor() {
   const [title, setTitle] = useState('');
-  
+  const [id, setId] = useState(0);
+  const [action, setAction] = useState(true);
+  const ajv = new Ajv();
+  //AJV
+  interface MyData {
+    name: string
+    email: string
+    password: string
+  }
+  const schema: JSONSchemaType<MyData> = {
+    type: 'object',
+    properties:{
+      name: {type:'string'},
+      email: {type:'string'},
+      password: {type:'string'}
+    },
+    required: ["name","email",'password'],
+    additionalProperties: false
+  }
+
+  const createDocument = () => {
+    if(action){
+      db.todoLists.add({ title });
+      setTitle('');  
+    }else{
+      db.todoLists.update(id,{
+        title,
+      });
+      setId(0);
+      setAction(true);
+      setTitle('');
+    }
+  }
     return (
       <div>
-        <label className="form-label">Editor de contenido (al finalizar de ingresar el contenido presiona la tecla Enter)</label><br/>
+        <label className="form-label">Editor de contenido</label><br/>
             <textarea 
                 className="form-control ta-editor" 
                 rows={7} 
                 autoComplete="off"
                 id="name"
+                value={title}
                 placeholder="Escribe un Schema válido, con la siguiente estructura:
                 name: string
                 email: string
@@ -22,14 +56,28 @@ export function YamlEditor() {
               onChange={ev => setTitle(ev.target.value)}
               onKeyUp={ev => {
                 if (ev.key === 'Enter') {
-                  db.todoLists.add({ title });
-                  setTitle('');
+                  if(action){
+                    db.todoLists.add({ title });
+                    setTitle('');  
+                  }else{
+                    db.todoLists.update(id,{
+                      title,
+                    });
+                    setId(0);
+                    setAction(true);
+                    setTitle('');
+                  }
                 }
               }}
               >
-                
             </textarea>
-            
+            <button className="btn btn-success" onClick={() => createDocument()}>Guardar schema</button>
+            <Error />
+            <YamlList 
+              recoveryTitle={ (title) => setTitle(title) }
+              recoveryId={ (id) => setId(id) }
+              recoveryAction={ (action) => setAction(action) }
+            />
       </div>
     );
 }
--- src/components/YamlList.tsx
+++ src/components/YamlList.tsx
@@ -1,15 +1,24 @@
 import { useLiveQuery } from "dexie-react-hooks";
-import * as React from "react";
+import {FC, Fragment} from "react";
 import { db } from "../models/db";
 import { YamlListItem } from "./YamlListItem";
 
+interface Props {
+  recoveryTitle: ( title: string ) => void;
+  recoveryId: ( id: number ) => void;
+  recoveryAction: ( action: boolean ) => void;
+}
 
+export const YamlList:FC<Props> = ({recoveryTitle, recoveryId, recoveryAction}) => {
+  
+  const editDocument = (list: any) => {
+    recoveryTitle(list.title);
+    recoveryId(list.id);
+    recoveryAction(false);
+  }
 
-export function YamlList() {
   const lists = useLiveQuery(() => db.todoLists.toArray());
-
   if (!lists) return null;
-
     return (
       <div>
         <br/>
@@ -24,7 +33,18 @@ export function YamlList() {
             </thead>
             <tbody>
               {lists.map(list => (
-                  <YamlListItem key={list.id} todoList={list}/>
+                <Fragment key={list.id}>
+                <tr>
+                  <th scope="row">{list.id}</th>
+                  <td>{list.title}</td>
+                  <td>
+                    <div className="btn-group" role="group" aria-label="Acciones">
+                      <button className="btn btn-primary" onClick={() => editDocument(list)} title="Editar documento">Editar</button>
+                      <button className="btn btn-dark" onClick={() => db.todoLists.delete(list.id)} title="Eliminar documento">Eliminar</button>
+                  </div>
+                  </td>
+                </tr>
+                </Fragment>
               ))}
             </tbody>
         </table>      
