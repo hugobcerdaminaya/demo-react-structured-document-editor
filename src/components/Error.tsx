c7cb9dcb29f995797dd178a459f2d81a1373a196
Hugo B. Cerda Minaya <hcerda@strappcorp.com>
2022-10-18 03:21:04 -0500

    define style display none for component error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export function Error() {
     return (
-      <div className="alert alert-danger" role="alert">
+      <div className="alert alert-danger" role="alert" style={{'display': 'none'}}>
         Ha ocurrido un error al validar el texto ingresado.
       </div>
     );
